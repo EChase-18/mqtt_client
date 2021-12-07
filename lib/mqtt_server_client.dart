da0d908d382e87ab5fa42b0d7d137c890abfbd2c
Ethan Chase <ethanchase@cmail.carleton.ca>
2021-12-07 12:38:32 -0500

    Tweaked server client with universal io

--- lib/mqtt_server_client.dart
+++ lib/mqtt_server_client.dart
@@ -9,11 +9,12 @@ library mqtt_server_client;
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
+// import 'dart:io';
 import 'dart:typed_data';
 import 'package:crypto/crypto.dart';
 import 'package:event_bus/event_bus.dart' as events;
 import 'package:typed_data/typed_data.dart' as typed;
+import 'package:universal_io/io.dart';
 import 'mqtt_client.dart';
 
 part 'src/connectionhandling/server/mqtt_client_mqtt_server_connection_handler.dart';
